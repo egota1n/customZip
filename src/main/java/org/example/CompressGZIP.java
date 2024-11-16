e231f657a7907b7711ce08f73275b963ccfb05bb
egota1n <rav266@inbox.ru>
2024-11-17 02:02:17 +0500

    Add gzip

---
+++ src/main/java/org/example/CompressGZIP.java
@@ -0,0 +1,48 @@
+package org.example;
+
+import java.io.FileInputStream;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.util.zip.GZIPOutputStream;
+
+public class CompressGZIP
+{
+    static final String OUTPUT_FILE
+            = "/Users/egortanachev/Documents/GitHub/customZip/compress.gz";
+    static final String INPUT_FILE
+            = "/Users/egortanachev/Documents/GitHub/customZip/compress.java";
+
+    static void compress()
+    {
+        byte[] buffer = new byte[1024];
+        try
+        {
+            GZIPOutputStream os =
+                    new GZIPOutputStream(new FileOutputStream(OUTPUT_FILE));
+
+            FileInputStream in =
+                    new FileInputStream(INPUT_FILE);
+
+            int totalSize;
+            while((totalSize = in.read(buffer)) > 0 )
+            {
+                os.write(buffer, 0, totalSize);
+            }
+
+            in.close();
+            os.finish();
+            os.close();
+
+            System.out.println("File Successfully compressed");
+        }
+        catch (IOException e)
+        {
+            e.printStackTrace();
+        }
+    }
+
+    public static void main (String[] args)
+    {
+        compress();
+    }
+}
---
+++ src/main/java/org/example/DecompressGZIP.java
@@ -0,0 +1,46 @@
+package org.example;
+
+import java.io.FileInputStream;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.util.zip.GZIPInputStream;
+
+public class DecompressGZIP {
+    static final String INPUT_FILE
+            = "/Users/egortanachev/Documents/GitHub/customZip/compress.gz";
+    static final String OUTPUT_FILE
+            = "/Users/egortanachev/Documents/GitHub/customZip/compress.java";
+
+    static void decompress()
+    {
+        byte[] buffer = new byte[1024];
+        try
+        {
+            GZIPInputStream is =
+                    new GZIPInputStream(new FileInputStream(INPUT_FILE));
+
+            FileOutputStream out =
+                    new FileOutputStream(OUTPUT_FILE);
+
+            int totalSize;
+            while((totalSize = is.read(buffer)) > 0 )
+            {
+                out.write(buffer, 0, totalSize);
+            }
+
+            out.close();
+            is.close();
+
+            System.out.println("File Successfully decompressed");
+        }
+        catch (IOException e)
+        {
+            e.printStackTrace();
+        }
+    }
+
+    public static void main (String[] args)
+    {
+        decompress();
+    }
+}
